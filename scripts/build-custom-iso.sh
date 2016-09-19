f534a022e4381660be0f388a0137c2e9eef37c7f
Alban Montaigu <alban.montaigu@gmail.com>
2016-09-19 22:08:54 +0200

    Added ```glibc-apps``` tcz to fix missing getent

--- scripts/build-custom-iso.sh
+++ scripts/build-custom-iso.sh
@@ -63,7 +63,7 @@ cd -
 rm -f "${EXTRACT_DIR}/initrd.xz"
 
 # Install our custom tcz
-for TCZ_PACKAGE in popt attr acl rsync; do
+for TCZ_PACKAGE in popt attr acl rsync glibc_apps; do
     curl -LO "${TCZ_URL}/${TCZ_PACKAGE}.tcz"; \
     mount -o loop "./${TCZ_PACKAGE}.tcz" "${MNT_TMP_DIR}"
     cd "${MNT_TMP_DIR}"
